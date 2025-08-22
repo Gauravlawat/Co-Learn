c3449fc8947bf9919949e903fe679757cafdef4d
Gauravlawat <gauravsoni32443@gmail.com>
2025-08-22 16:44:15 +0530

    feat: Add production CORS origin

--- server/server.js
+++ server/server.js
@@ -8,6 +8,14 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
+const cors = require('cors');
+
+// This tells your server to only accept requests from your live Vercel site
+app.use(cors({
+    origin: 'https://co-learn-nine.vercel.app'
+}));
+
+
 const io = new Server(server, {
     cors: {
         origin: process.env.CLIENT_URL,
